35e06b1a0436ead68deff8d036b53914061d6fc5
Jordan Pang <jordan.pang@rmaict.com>
2024-03-25 16:55:45 +0800

    Added login authentication with database

--- LoginAPI/LoginAPI/Controllers/AuthController.cs
+++ LoginAPI/LoginAPI/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using LoginAPI.Models;
+﻿using LoginAPI.Data;
+using LoginAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -11,21 +12,27 @@ namespace LoginAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public static User user = new User();
         private readonly IConfiguration _configuration;
+        private readonly AppDbContext _context;
 
-        public AuthController(IConfiguration configuration)
+        public AuthController(IConfiguration configuration, AppDbContext context)
         {
             _configuration = configuration;
+            _context = context;
         }
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserDto request)
         {
+            User user = new User();
             string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
             user.Username = request.Username;
             user.Password = passwordHash;
+
+            _context.Users.Add(user);
+
+            _context.SaveChanges();
 
             return Ok(user);
         }
@@ -33,20 +40,27 @@ namespace LoginAPI.Controllers
         [HttpPost("login")]
         public ActionResult<User> Login(UserDto request)
         {
-            if (user.Username != request.Username)
+            // Query the database for the user with the requested username
+            User? user = _context.Users.FirstOrDefault(u => u.Username == request.Username);
+
+            // If the user is not found, return a "User not found" error
+            if (user == null)
             {
                 return BadRequest("User not found.");
             }
 
+            // If the password is not correct, return an "Invalid password" error
             if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
             {
                 return BadRequest("Invalid password.");
             }
 
+            // If the username and password are correct, create a token for the user
             string token = CreateToken(user);
 
             return Ok(token);
         }
+
 
         private string CreateToken(User user)
         {
--- LoginAPI/LoginAPI/Models/User.cs
+++ LoginAPI/LoginAPI/Models/User.cs
@@ -1,7 +1,10 @@
-﻿namespace LoginAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginAPI.Models
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
